ad681fa8af563b840a3c56d38e14a4c7a6576c38
Raphael Adams <raphael.adams@shopify.com>
2020-11-02 10:35:08 -0500

    First commit for Textogram

---
+++ tests/test_textogram.rb
@@ -0,0 +1,8 @@
+# Textogram
+# Raphael Adams
+
+require 'minitest/autorun'
+require_relative '../textogram'
+
+class TestTextogram < Minitest::Test
+end
---
+++ textogram.rb
@@ -0,0 +1,16 @@
+# Textogram
+# Raphael Adams
+
+class Textogram
+  attr_reader :input
+  attr_reader :case_sensitive
+  attr_reader :special_characters
+  attr_reader :split_character
+  
+  def initialize(input, case_sensitive = "N", special_characters = "Y", split_character = " ")
+  end
+
+  def to_s
+    # print the histogram, characters should be sorted
+  end
+end
