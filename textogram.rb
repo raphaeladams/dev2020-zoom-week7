28fe9ed59512e0cad743136b044908e6735dbb98
Raphael Adams <raphael.adams@shopify.com>
2020-11-02 12:13:26 -0500

    Added code to generate, sort, and print histogram

--- textogram.rb
+++ textogram.rb
@@ -4,18 +4,40 @@
 class Textogram
   attr_reader :input
   attr_reader :case_sensitive
-  attr_reader :special_characters
+  attr_reader :incl_special_characters
   attr_reader :split_character
 
-  def initialize(input, case_sensitive = "N", special_characters = "Y", numbers = "Y")
+  def initialize(input, case_sensitive = "N", incl_special_characters = "Y", incl_numbers = "Y")
+
     @input = input.to_s
     @case_sensitive = verify_specs(case_sensitive)
-    @special_characters = verify_specs(special_characters)
-    @numbers = verify_specs(numbers)
+    @incl_special_characters = verify_specs(incl_special_characters)
+    @incl_numbers = verify_specs(incl_numbers)
+
+    @input.downcase! unless @case_sensitive
+    @content_for_histogram = @input.split(//)
+
+    @content_for_histogram.reject! { |i| i[/\s/] }
+    @content_for_histogram.reject! { |i| i[/\W/] } unless @incl_special_characters
+    @content_for_histogram.reject! { |i| i[/\d/] } unless @incl_numbers
+
+    @histogram = Hash.new
+
+    @content_for_histogram.each do |i|
+      key = i.to_sym
+      @histogram[key] == nil ? @histogram[key] = 1 : @histogram[key] += 1
+    end
+    
   end
 
   def to_s
     # print the histogram, characters should be sorted
+    @printout = @histogram.sort
+    @printout.each do |i|
+      print "#{ i.first } "
+      i.last.times { print "*" }
+      print "\n"
+    end
   end
 
   def verify_specs(specification)
@@ -29,3 +51,7 @@ class Textogram
   end
 
 end
+
+test_textogram = Textogram.new("HELLO World!", "N", "Y", "Y")#("aoS!?ien2o3rn", "Y", "Y", "Y")
+test_textogram.to_s
+puts "\n"
