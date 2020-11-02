b16690c27c76d0e2c4dbdd819c3d9d2b4defc04c
Raphael Adams <raphael.adams@shopify.com>
2020-11-02 16:22:27 -0500

    Now generating histograms for letters and words; added further tests; read in Gutenberg file

--- tests/test_textogram.rb
+++ tests/test_textogram.rb
@@ -7,78 +7,82 @@ require_relative '../textogram'
 class TestHistogram < Minitest::Test
   def test_case_sensitivity
     test_textogram = Textogram.new("Hw")
-    assert_equal test_textogram.histogram, { h: 1, w: 1 }
+    assert_equal test_textogram.histogram_letters, { h: 1, w: 1 }
 
     test_textogram = Textogram.new("Hw", true)
-    assert_equal test_textogram.histogram, { H: 1, w: 1 }
+    assert_equal test_textogram.histogram_letters, { H: 1, w: 1 }
 
     test_textogram = Textogram.new("Hh")
-    assert_equal test_textogram.histogram, { h: 2 }
+    assert_equal test_textogram.histogram_letters, { h: 2 }
 
     test_textogram = Textogram.new("Hh", true)
-    assert_equal test_textogram.histogram, { H: 1, h: 1 }
+    assert_equal test_textogram.histogram_letters, { H: 1, h: 1 }
   end
 
   def test_including_special_chars
     test_textogram = Textogram.new("Hw!", false, true)
-    assert_equal test_textogram.histogram, { h: 1, w: 1, '!': 1 }
+    assert_equal test_textogram.histogram_letters, { h: 1, w: 1, '!': 1 }
   end
 
   def test_rejecting_special_chars
     test_textogram = Textogram.new("Hw!", false, false)
-    assert_equal test_textogram.histogram, { h: 1, w: 1 }
+    assert_equal test_textogram.histogram_letters, { h: 1, w: 1 }
   end
 
   def test_including_numbers
     test_textogram = Textogram.new("Hw1", false, false, true)
-    assert_equal test_textogram.histogram, { h: 1, w: 1, '1': 1 }
+    assert_equal test_textogram.histogram_letters, { h: 1, w: 1, '1': 1 }
   end
 
   def test_rejecting_numbers
     test_textogram = Textogram.new("Hw1", false, false, false)
-    assert_equal test_textogram.histogram, { h: 1, w: 1 }
+    assert_equal test_textogram.histogram_letters, { h: 1, w: 1 }
   end
 
   def test_empty_strings
     test_textogram = Textogram.new("", false, false, false)
-    assert_equal test_textogram.histogram, { }
+    assert_equal test_textogram.histogram_letters, { }
   end
 
   def test_input_with_all_numbers
     test_textogram = Textogram.new(2013, false, false, true)
-    assert_equal test_textogram.histogram, { '0': 1, '1': 1, '2': 1, '3': 1 }
+    assert_equal test_textogram.histogram_letters, { '0': 1, '1': 1, '2': 1, '3': 1 }
 
     test_textogram = Textogram.new(2013, false, false, false)
-    assert_equal test_textogram.histogram, { }
+    assert_equal test_textogram.histogram_letters, { }
   end
 
   def test_input_with_all_symbols
     test_textogram = Textogram.new("?!*$", false, true)
-    assert_equal test_textogram.histogram, { '?': 1, '!': 1, '*': 1, '$': 1 }
+    assert_equal test_textogram.histogram_letters, { '?': 1, '!': 1, '*': 1, '$': 1 }
 
     test_textogram = Textogram.new("?!*$", false, false)
-    assert_equal test_textogram.histogram, { }
+    assert_equal test_textogram.histogram_letters, { }
   end
 end
+
 
 class TestOutput < Minitest::Test
   def test_no_case_sensitivity_and_a_symbol
     test_textogram = Textogram.new("HELLO World!", false, true, true)
-    assert_output("! *\nd *\ne *\nh *\nl ***\no **\nr *\nw *\n") { test_textogram.to_s }
+    assert_output(/!\s+\* | d\s+\* | e\s+\* | h\s+\* | l\s+\*\*\* | o\s+\*\* | r\s+\* | w\s+\*/x) { test_textogram.to_s }
+    assert_output(/ hello\s+\*  |  world!\s+\* /x) { test_textogram.to_s }
   end
 
   def test_case_sensitivity_and_symbols_and_no_numbers
-    test_textogram = Textogram.new("aoS!?ien2o3rn", true, true, false)
-    assert_output("! *\n? *\nS *\na *\ne *\ni *\nn **\no **\nr *\n") { test_textogram.to_s }
+    test_textogram = Textogram.new("aS!?2o3rn", true, true, false)
+    assert_output(/!\s+\* | \?\s+\* | S\s+\* | a\s+\* | n\s+\*\* | o\s+\*\* | r\s+\*/x) { test_textogram.to_s }
+    assert_output(/WORDS:\nNothing to print/) { test_textogram.to_s }
   end
 
   def test_case_sensitivity_and_symbols_and_numbers
-    test_textogram = Textogram.new("aoS!?ien2o3rn", true, true, true)
-    assert_output("! *\n2 *\n3 *\n? *\nS *\na *\ne *\ni *\nn **\no **\nr *\n") { test_textogram.to_s }
+    test_textogram = Textogram.new("aS!?2o3r", true, true, true)
+    assert_output(/!\s+\* | 2\s+\* | 3\s+\* | \?\s+\* | S\s+\* | a\s+\* | o\s+\*\* | r\s+\*/x) { test_textogram.to_s }
+    assert_output(/ aS!\?2o3r\s+\* /x) { test_textogram.to_s }
   end
 
   def test_empty_string
     test_textogram = Textogram.new("", true, true, false)
-    assert_output("") { test_textogram.to_s }
+    assert_output(/LETTERS:\nNothing to print\n\nWORDS:\nNothing to print/) { test_textogram.to_s }
   end
 end
--- textogram.rb
+++ textogram.rb
@@ -6,7 +6,8 @@ class Textogram
   attr_reader :case_sensitive
   attr_reader :incl_special_characters
   attr_reader :incl_numbers
-  attr_reader :histogram
+  attr_reader :histogram_letters
+  attr_reader :histogram_words
 
   def initialize(input, case_sensitive = false, incl_special_characters = true, incl_numbers = true)
 
@@ -16,29 +17,56 @@ class Textogram
     @incl_numbers = incl_numbers
 
     @input.downcase! unless @case_sensitive
-    @content_for_histogram = @input.split(//)
 
-    @content_for_histogram.reject! { |i| i[/\s/] }
-    @content_for_histogram.reject! { |i| i[/\W/] } unless @incl_special_characters
-    @content_for_histogram.reject! { |i| i[/\d/] } unless @incl_numbers
-
-    @histogram = Hash.new
-
-    @content_for_histogram.each do |i|
-      key = i.to_sym
-      @histogram[key] == nil ? @histogram[key] = 1 : @histogram[key] += 1
-    end
-    @histogram
+    @histogram_letters = generate_histogram(@input.split(//))
+    @histogram_words = generate_histogram(@input.split(" "))
 
   end
 
+  public
+
   def to_s
-    @printout = @histogram.sort
-    @printout.each do |i|
-      print "#{ i.first } "
+    puts "\nLETTERS:"
+    print_histogram(@histogram_letters)
+    puts "\nWORDS:"
+    print_histogram(@histogram_words)
+  end
+
+  private
+
+  def generate_histogram(array)
+    array.reject! { |i| i[/\s/] }
+    array.reject! { |i| i[/\W/] } unless @incl_special_characters
+    array.reject! { |i| i[/\d/] } unless @incl_numbers
+
+    histogram = Hash.new
+
+    array.each do |i|
+      key = i.to_sym
+      histogram[key] == nil ? histogram[key] = 1 : histogram[key] += 1
+    end
+    
+    histogram
+  end
+
+  def print_histogram(array)
+    if array.empty?
+      puts "Nothing to print"
+      return
+    end
+
+    printout = array.sort
+    printout.each do |i|
+      print "#{ i.first } ".ljust(10)
       i.last.times { print "*" }
       print "\n"
     end
   end
 
 end
+
+
+project_gutenberg_textogram = Textogram.new(File.read("GreekBiology_Singer.txt").strip, false, false, false)
+puts project_gutenberg_textogram.histogram_letters
+puts project_gutenberg_textogram.histogram_words
+project_gutenberg_textogram.to_s
